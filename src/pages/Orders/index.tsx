f8c8ff658a03408a462920a8a9d3574db91c9403
Leandro Rocha <prog.leandro@gmail.com>
2020-06-25 23:14:24 -0300

    Implementando a página de favoritos

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -32,7 +32,14 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      const ordersResult = await api.get('orders');
+
+      const ordersFormatted = ordersResult.data.map((item: Food) => ({
+        ...item,
+        formattedPrice: formatValue(item.price),
+      }));
+
+      setOrders([...ordersFormatted]);
     }
 
     loadOrders();
